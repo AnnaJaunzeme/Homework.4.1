4efd4ce25cb2975ea079da64b417a20bee2d5c14
Anna Jaunzeme <anna.jaunzeme@inbox.lv>
2021-10-18 14:02:52 +0300

    Homework.4.1.

--- Homework.4.1/Program.cs
+++ Homework.4.1/Program.cs
@@ -10,51 +10,58 @@ namespace Homework._4._1
             int Number2;                   
             string operation;
             int answer;
+            
+            
 
             
 
-            Console.Write("Please input the first number: ");
+                Console.Write("Please input the first number: ");
             Number1= Convert.ToInt32(Console.ReadLine());
 
-            
-            Console.Write("Now enter your second number: ");
-            Number2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Ok, now enter your operation ( x , / , +, -): ");
+            Console.Write("Ok, now enter your operation ( x , / , +, -, Exit): ");
             operation = Console.ReadLine();
 
-            if (operation == "x")
-            {
-                answer = Number1 * Number2;
-                Console.WriteLine(Number1 + " x " + Number2 + " = " + answer);
-                Console.ReadLine();
-            }
-            else if (operation == "/")
-            {
-                answer = Number1 / Number2;
-                Console.WriteLine(Number1 + " / " + Number2 + " = " + answer);
-                Console.ReadLine();
-            }
-                        else if (operation == "+")
-            {
-                answer = Number1 + Number2;
-                Console.WriteLine(Number1 + " + " + Number2 + " = " + answer);
-                Console.ReadLine();
-            }
-            else if (operation == "-")
-            {
-                answer = Number1 - Number2;
-                Console.WriteLine(Number1 + " - " + Number2 + " = " + answer);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Sorry that is not correct format!\n Please try again!");    
-                Console.ReadLine();
-            }
+            Console.Write("Now enter your second number: ");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            
+
+
+                switch (operation)
+                {
+                    case "x":
+                        answer = Number1 * Number2;
+                        Console.WriteLine(Number1 + " x " + Number2 + " = " + answer);
+                        Console.ReadLine();
+                        break;
+                    case "/":
+                        answer = Number1 / Number2;
+                        Console.WriteLine(Number1 + " / " + Number2 + " = " + answer);
+                        Console.ReadLine();
+                        break;
+                    case "+":
+                        answer = Number1 + Number2;
+                        Console.WriteLine(Number1 + " + " + Number2 + " = " + answer);
+                        Console.ReadLine();
+                        break;
+                    case "-":
+                        answer = Number1 - Number2;
+                        Console.WriteLine(Number1 + " - " + Number2 + " = " + answer);
+                        Console.ReadLine();
+                        break;
+                    case "Exit":
+                        Console.WriteLine("Press Enter to exit the program!");
+                        break;
+                    default:
+                        Console.WriteLine("Sorry that is not correct format!\n Please try again!");
+                        Console.ReadLine();
+                        break;
+                }
+            
             Console.ReadLine();
         }
+                                            
+        }
     }
-}
         
     
 
